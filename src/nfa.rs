9b01f646adab289a5f5dcaa4a889a3b5a83a995d
Kaede Fujisaki <kaede@hexe.net>
2022-04-07 08:32:05 +0900

    create struct

---
+++ src/nfa.rs
@@ -0,0 +1,17 @@
+use std::hash::Hash;
+
+pub struct Spec<Stat, Trans>
+  where
+    Stat: Eq + Hash + Clone,
+    Tran: Eq + Hash + Clone,
+{
+
+}
+
+impl <Stat, Tran> Spec <Stat, Tran>
+  where
+    Stat: Eq + Hash + Clone,
+    Tran: Eq + Hash + Clone,
+{
+
+}
