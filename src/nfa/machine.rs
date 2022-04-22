cf2eaf371b3cf4b3c7fe42c86b0adb2bac02e194
Kaede Fujisaki <kaede@hexe.net>
2022-04-22 22:54:40 +0900

    add from_spec

--- src/nfa/machine.rs
+++ src/nfa/machine.rs
@@ -1,3 +1,26 @@
-pub struct Machine {
+use std::collections::HashSet;
+use std::fmt::Debug;
+use std::hash::Hash;
+use super::spec::{Spec, Input};
 
+pub struct Machine<Stat, Token>
+  where
+    Stat: Eq + Hash + Clone,
+    Token: Eq + Hash + Clone + Debug,
+{
+  spec: Spec<Stat, Token>,
+  states: HashSet<Stat>,
+}
+
+impl <Stat, Token> Machine<Stat, Token>
+  where
+    Stat: Eq + Hash + Clone,
+    Token: Eq + Hash + Clone + Debug,
+{
+  pub fn from_spec(spec: Spec<Stat, Token>) -> Self {
+    Self {
+      spec,
+      states: HashSet::new(),
+    }
+  }
 }
