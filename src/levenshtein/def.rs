e6510a5cb7f19b6d585eda3773e42360a73e486c
Kaede Fujisaki <kaede@hexe.net>
2022-05-04 08:49:30 +0900

    add test

--- src/levenshtein/def.rs
+++ src/levenshtein/def.rs
@@ -1,21 +1,33 @@
+use std::cmp::min;
 use std::ops::{Index, IndexMut};
 
 pub fn distance<'a>(a: impl Into<&'a str>, b: impl Into<&'a str>) -> usize {
-  let a = a.into();
-  let b = b.into();
-  let mut table = Table::<usize>::new(a.len(), b.len());
-  for x in 0..a.len() {
+  let a = a.into().as_bytes();
+  let b = b.into().as_bytes();
+  let mut table = Table::<usize>::new(a.len() + 1, b.len() + 1);
+  for x in 1..=a.len() {
     table[(x, 0)] = x;
   }
-  for y in 0..b.len() {
+  for y in 1..=b.len() {
     table[(0, y)] = y;
   }
-  for y in 1..=table.height() {
-    for x in 1..=table.width() {
-
+  for y in 1..table.height() {
+    for x in 1..table.width() {
+      let d = if a[x-1] == b[y-1] {
+        min(
+          min(table[(x-1, y-1)], table[(x-1, y)] + 1),
+          table[(x, y-1)] + 1
+        )
+      } else {
+        min(
+          min(table[(x-1, y-1)] + 2, table[(x-1, y)] + 1),
+          table[(x, y-1)] + 1
+        )
+      };
+      table[(x,y)] = d;
     }
   }
-  0
+  table[(a.len(), b.len())]
 }
 
 struct Table<T>
@@ -74,7 +86,19 @@ mod test {
   }
 
   #[test]
-  fn basic() {
-    assert_eq!(2, distance("zoi", "koi"))
+  fn identity() {
+    assert_eq!(0, distance("koi", "koi"))
+  }
+  #[test]
+  fn replace() {
+    assert_eq!(2, distance("koi", "zoi"))
+  }
+  #[test]
+  fn insert() {
+    assert_eq!(1, distance("koi", "kooi"))
+  }
+  #[test]
+  fn delete() {
+    assert_eq!(1, distance("koi", "ki"))
   }
 }
